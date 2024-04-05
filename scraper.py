c0754738dd2327ceb859b365322ee50759057168
Lee Jia Keat <leejiakeat@gmail.com>
2024-04-05 21:41:11 +0800

    hehe

--- scraper.py
+++ scraper.py
@@ -4,17 +4,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
 
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+
+options = Options()
+ua = UserAgent()
+user_agent = ua.random
+print(user_agent)
+
+options.add_argument(f'--user-agent={user_agent}')
+driver = webdriver.Chrome(options=options)
+
+import time;
+import random;
+
 driver = webdriver.Chrome()
 driver.get("https://sg.indeed.com/jobs?q=software+engineer+entry+level&l=Singapore")
-
+driver.maximize_window()
 
 title = driver.title
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 60)
 
 acceptCookiesButtonLocator = (By.ID, "onetrust-accept-btn-handler")
 wait.until(EC.element_to_be_clickable(acceptCookiesButtonLocator)).click()
 
 jobs_with_salary = []
+
+def clamp(x, a, b):
+    return max(a, min(x, b))
+
+def sleep_random(min_sleep_time, max_sleep_time): 
+    sleep_time = random.random() * max_sleep_time
+    sleep_time_clamped = clamp(sleep_time, min_sleep_time, max_sleep_time)
+    
+    time.sleep(sleep_time_clamped)
 
 while True:
     jobs = driver.find_elements(By.CLASS_NAME, "tapItem")
@@ -23,6 +46,8 @@ while True:
         
         wait.until(EC.element_to_be_clickable(job))
         job.click()
+        
+        sleep_random(2, 3)
         
         try:
             jobPane = driver.find_element(By.ID, "jobsearch-ViewjobPaneWrapper")
@@ -41,8 +66,7 @@ while True:
         
         try:
             payElement = driver.find_element(By.XPATH, "//div[@aria-label='Pay']")
-            payText = payElement.text.strip()
-
+            payText = payElement.text.strip().removeprefix("Pay\n")
             
             jobs_with_salary.append((jobTitleText, companyNameText, payText))
             print(payText + " (" + jobTitleText + ")" + " - " + companyNameText)
@@ -51,6 +75,7 @@ while True:
             print("No pay found for job: " + jobTitle.text.strip() + " (" + companyName.text.strip() + ")")
                 
     nextPageLinkLocator = (By.CSS_SELECTOR, "a[data-testid='pagination-page-next']")
+    sleep_random(2, 3)
     wait.until(EC.element_to_be_clickable(nextPageLinkLocator)).click()
                 
     for job in jobs_with_salary:
